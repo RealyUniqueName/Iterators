3e52bef36ddf0ea4be725b08d712f204698840e3
Alexander Kuzmenko <alex@stablex.ru>
2018-02-06 15:34:55 +0300

    fix typing

--- src/iterators/items/KeyValuePair.hx
+++ src/iterators/items/KeyValuePair.hx
@@ -2,9 +2,9 @@ package iterators.items;
 
 class KeyValuePair<K,V> {
 	public var key (default,null):K;
-	public var value (default,null):Null<V>;
+	public var value (default,null):V;
 
-	public inline function new(key:K, value:Null<V>) {
+	public inline function new(key:K, value:V) {
 		this.key = key;
 		this.value = value;
 	}
